699a4287890fa20716bbebefeb1e5ff9dfd9515e
0322203936-cmd <0322203936@ut-tijuana.edu.mx>
2026-04-19 18:38:26 -0700

    Update data_extractor.py

--- data_extractor.py
+++ data_extractor.py
@@ -19,7 +19,7 @@ from io import BytesIO
 # Archivo principal: hojas WK####
 SHAREPOINT_URL_WK = (
     "https://pacificafarms-my.sharepoint.com/:x:/g/personal/"
-    "anahi_mora_cfbc_co/IQAQCb79SzHtRrTQR71pSNQcASOWqFXyeGGzEhUcT9FRRJ4?e=J9gsiA"
+    "anahi_mora_cfbc_co/IQAQCb79SzHtRrTQR71pSNQcASOWqFXyeGGzEhUcT9FRRJ4?e=wEgBRm"
 )
 # Archivo secundario: hojas PR####, MP####, ME####
 SHAREPOINT_URL_PR = (
